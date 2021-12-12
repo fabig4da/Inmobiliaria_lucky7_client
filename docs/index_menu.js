c801d701d317902985e33d4b49cc9d8261dc0c84
fabian <fabig4da@gmail.com>
2021-12-12 00:28:39 -0500

    docs folder added

--- docs/index_menu.js
+++ docs/index_menu.js
@@ -2,7 +2,7 @@ window.onload = function() {
     let sidebar = document.querySelector(".sidebar");
     // console.log(sidebar);
     let btn = document.querySelector("#btn");
-    // console.log(btn);
+    // / console.log(btn);
     btn.onclick = function() {
         sidebar.classList.toggle("active");
     };
